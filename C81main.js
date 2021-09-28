a0d7b8214ef932d5f73a0feee635922d311ba6cb
chhavi-code <76948763+chhavi-code@users.noreply.github.com>
2021-09-28 19:54:03 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,30 @@
+canvas=document.getElementById("myCanvas")
+ctx=canvas.getContext("2d")
+color="red"
+ctx.beginPath();
+ctx.strokesStyle=color
+ctx.lineWidth=5
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mouseDown",MyMouseDown);
+
+
+function MyMouseDown(e)
+{
+    mousex=e.clientX-canvas.offsetLeft
+    mousey=e.clientY-canvas.offsetTop
+ 
+   console.log("x=,"+mousex + ", y=" + mousey)
+
+   circle(mousex,mousey);
+}
+
+function circle(mousex,mousey)
+{
+
+    ctx.beginPath();
+ctx.strokesStyle=color
+ctx.lineWidth=5
+ctx.arc(mousex,mousey,40,0,2*Math.PI);
+ctx.stroke();
+}
