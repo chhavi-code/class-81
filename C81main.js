93abbd438658f462504f5999485201bc58ec1959
chhavi-code <76948763+chhavi-code@users.noreply.github.com>
2021-09-28 19:56:49 +0530

    Update C81main.js

--- C81main.js
+++ C81main.js
@@ -2,7 +2,7 @@ canvas=document.getElementById("myCanvas")
 ctx=canvas.getContext("2d")
 color="red"
 ctx.beginPath();
-ctx.strokesStyle=color
+ctx.strokeStyle=color
 ctx.lineWidth=5
 ctx.arc(200,200,40,0,2*Math.PI);
 ctx.stroke();
